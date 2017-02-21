9a0e7d8279ecc67eccb7cc1dc2528421b8ccad2c
amvaleh <amvaleh@gmail.com>
2017-02-21 17:13:00 +0330

    config for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'khetabeghadir.com' }
 end
