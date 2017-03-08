5c2aecc42a3919a0670e03e92a9553c78c4f91ab
amvaleh <amvaleh@gmail.com>
2017-03-09 00:57:12 +0330

    recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -3,5 +3,4 @@ Recaptcha.configure do |config|
   config.secret_key = '6LfiehcUAAAAADBNetuRRfL5NR8XKH_gsySozvT9'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
-  config.api_version = 'v2'
 end
