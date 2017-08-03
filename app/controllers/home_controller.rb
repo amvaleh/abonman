577814bc2a21ce227da03d63c43c904c06566134
amvaleh <amvaleh@gmail.com>
2017-08-03 13:29:10 +0430

    final list with 0 nums

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -42,7 +42,8 @@ class HomeController < ApplicationController
       if persons.any?
         person = persons.first
         p = "#{person.gender_fa}#{person.name} \\\\n رمز عبور جدید شما در آبونمان خطابه غدیر و فدک: \\\\n #{person.generate_password} \\\\n شماره موبایل: \\\\n #{person.mobile_number} \\\\n http://ab.khetabeghadir.com/profile \\\\n یا علی."
-        send_cron_msg(person,p)
+        kh = ""
+        send_cron_msg(person,p,kh)
         redirect_to "#{root_path}#profile" , :alert => "رمز جدید برای شما ارسال شد."
       else
         redirect_to "#{root_path}#profile" , :alert => "شماره موبایل #{number} تا به حال در سیستم ثبت نشده است."
